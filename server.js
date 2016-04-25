a605d2d8a82f3a2314e61cec92b54b15d9ef0cb5
Stephen <stephenlallum@gmail.com>
2016-04-25 12:35:06 -1000

    added more files

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var methodOverride = require('method-override');
 var db = require('./models');
 var Gallery = db.Gallery;
 
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(methodOverride(function (req, res){
+  if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.post('/gallery', function (req, res) {
   Gallery.create({
@@ -25,6 +37,8 @@ app.get('/gallery', function (req, res) {
       res.json(gallery);
     });
 });
+
+
 
 app.listen(3000, function() {
   db.sequelize.sync();
